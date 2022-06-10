fdfb59a38269b0b02b16c1f40d6cf6795dd6ea2e
Luiz Filipy <luizfilipy.aa@outlook.com>
2022-06-10 14:58:34 -0300

    Initial commit

---
+++ src/main/java/com/github/lf2a/DemoClass.java
@@ -0,0 +1,21 @@
+package com.github.lf2a;
+
+public class DemoClass {
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return a string representation of the object.
+     */
+    public String getName() {
+        return "Luiz";
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return a string representation of the object.
+     */
+    public static String getNameStatic() {
+        return "Luiz";
+    }
+}
